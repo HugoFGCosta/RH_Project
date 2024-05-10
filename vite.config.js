8fd9c661681af1ddc9891319911578b1a2d78d52
HugoFGCosta <hugo.costa.T0126211@edu.atec.pt>
2024-05-10 20:15:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
-                'resources/css/app.css'
+                'resources/css/app.css',
             ],
             refresh: true,
         }),
@@ -21,8 +25,5 @@ export default defineConfig({
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
         },
-    },
-    build: {
-        outDir: 'public/build',
     },
 });
