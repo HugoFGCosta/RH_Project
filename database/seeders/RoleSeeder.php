529c88333fe20f0a10d18a4d1d7c27631d2c71f6
Hugo Costa <140256666+HugoFGCosta@users.noreply.github.com>
2024-05-11 09:22:10 +0100

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -12,21 +13,13 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-
-
-        \DB::table('roles')->insert([
-            'id' => 1,
-            'role' => 'Worker'
-        ]);
-
-        \DB::table('roles')->insert([
-            'id' => 2,
-            'role' => 'Manager'
-        ]);
-
-        \DB::table('roles')->insert([
-            'id' => 3,
-            'role' => 'Administrator'
-        ]);
+        // Verificar se a tabela roles já contém os dados antes de inserir
+        if (DB::table('roles')->count() == 0) {
+            DB::table('roles')->insert([
+                ['id' => 1, 'role' => 'Worker'],
+                ['id' => 2, 'role' => 'Manager'],
+                ['id' => 3, 'role' => 'Administrator']
+            ]);
+        }
     }
 }
