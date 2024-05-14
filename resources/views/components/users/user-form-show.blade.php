c281b69aff9aa9baf075e57f6e7d8d00f093e854
HugoFGCosta <hugo.costa.T0126211@edu.atec.pt>
2024-05-14 13:11:01 +0100

    update form

--- resources/views/components/users/user-form-show.blade.php
+++ resources/views/components/users/user-form-show.blade.php
@@ -3,7 +3,6 @@
 <form method="SHOW" action="{{ url('users') }}" enctype="multipart/form-data">
     @csrf
     <div class="form-group">
-        <label for="name">Name</label>
         <input type="text" id="name" name="name" autocomplete="name" placeholder="Type your name"
             class="form-control" value="{{ isset($user->name) ? $user->name : '' }}" required
             aria-describedby="nameHelp" disabled>
