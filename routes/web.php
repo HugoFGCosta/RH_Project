4cc7e61b656620acc47d73031ac6de0995c94b6a
Caue Furayama <cauefurayama@gmail.com>
2024-05-23 13:32:18 +0100

    Caue - Calendario Funcional

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminRegisterController;
 use App\Http\Controllers\AuthenticatedRegisterController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\PresenceController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
@@ -84,3 +85,12 @@ Route::get('export/vacations', [\App\Http\Controllers\VacationController::class,
 
 Route::post('import/presences', [\App\Http\Controllers\PresenceController::class, 'import'])->name('importPresences');
 Route::get('export/presences', [\App\Http\Controllers\PresenceController::class, 'export'])->name('exportPresences');
+
+
+
+/* Rota CALENDARIO */
+Route::controller(EventController::class)->group(function () {
+    Route::get('fullcalender', 'index');
+    Route::post('fullcalenderAjax', 'ajax');
+})->middleware('check.calendar');
+;
