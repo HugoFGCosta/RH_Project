3719ffb13b2bf73040c5f909612a5ce80611e5fc
Luismcs <fcpluismcs12@gmail.com>
2024-07-03 17:15:20 +0100

    Luis- Factories Users e WorkShifts

--- database/seeders/AbsenceSeeder.php
+++ database/seeders/AbsenceSeeder.php
@@ -13,7 +13,7 @@ class AbsenceSeeder extends Seeder
      */
     public function run(): void
     {
-
+        /*
         // Verificar se a tabela work_shifts já contém os dados antes de inserir
         if (DB::table('absences')->count() == 0) {
             DB::table('absences')->insert([
@@ -44,5 +44,6 @@ class AbsenceSeeder extends Seeder
 
             ]);
         }
+        */
     }
 }
