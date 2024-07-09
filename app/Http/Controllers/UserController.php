6c37b38e173ed7af8d6fef593ef93d4f763d68c6
Hugo Costa <140256666+HugoFGCosta@users.noreply.github.com>
2024-07-09 17:33:25 +0100

    alteração da frase do destroy admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -249,7 +249,7 @@ class UserController extends Controller
 
         // Verifica se o utilizador se está a apagar a si mesmo
         if($user->id == auth()->user()->id){
-            return redirect('/users/show-all')->with('error', 'Não se pode apagar a si mesmo! Por favor, troque para outro administrador e tente novamente.');
+            return redirect('/users/show-all')->with('error', 'Não se pode apagar a si mesmo! Por favor, solicite a outro administrador.');
         }
 
         // Verifica se o utilizador é o único administrador
