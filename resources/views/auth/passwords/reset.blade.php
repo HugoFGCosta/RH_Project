6b8d621d20e5a4911f04ef7ab7e9b5bbdbb699ef
Caue Furayama <cauefurayama@gmail.com>
2024-07-17 09:04:01 +0100

    Falta ajustar o reset password css

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,52 +3,56 @@
 @section('content')
     <div class="container">
         <div class="Form recoverPassword-form">
-            {{--                    <div class="card-header">{{ __('Reset Password') }}</div>--}}
+            {{--                    <div class="card-header">{{ __('Reset Password') }}</div> --}}
             <img src="{{ asset('images/hospital.svg') }}" alt="" style="width: 100%; height: auto;" class="p-2 mt-5">
             <h4 class="mt-5 mb-4">Recuperar Palavra-Passe</h4>
-                        <form method="POST" action="{{ route('password.update') }}">
-                            @csrf
+            <form method="POST" action="{{ route('password.update') }}">
+                @csrf
 
-                            <input type="hidden" name="token" value="{{ $token }}">
+                <input type="hidden" name="token" value="{{ $token }}">
 
-                            <div class="input-container mt-4 align-items-start">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-                                <label for="email">{{ __('Email') }}</label>
+                <div class="input-container mt-4 align-items-start">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                        name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                    <label for="email">{{ __('Email') }}</label>
 
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-
-
-
-
-                                <div class="input-container mt-5">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                    <label for="password">{{ __('Nova Password') }}</label>
-                                    <img src="{{ asset('images/eye-closed.png') }}" id="eyeicon">
-                                </div>
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-
-                                <div class="input-container mt-5">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                    <label for="password-confirm">{{ __('Confirmar Password') }}</label>
-                                    <img src="{{ asset('images/eye-closed.png') }}" id="eyeicon">
-                                </div>
-
-
-                                    <button type="submit" class="btn">
-                                        {{ __('Confirmar') }}
-                                    </button>
-                        </form>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-            </div>
+
+
+
+
+                <div class="input-container mt-5">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                        name="password" required autocomplete="new-password">
+                    <label for="password">{{ __('Nova Password') }}</label>
+                    <img src="{{ asset('images/eye-closed.png') }}" id="eyeicon">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="input-container mt-5">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required
+                        autocomplete="new-password">
+                    <label for="password-confirm">{{ __('Confirmar Password') }}</label>
+                    <img src="{{ asset('images/eye-closed.png') }}" id="eyeicon">
+                </div>
+
+
+                <button type="submit" class="btn">
+                    {{ __('Confirmar') }}
+                </button>
+            </form>
+        </div>
+    </div>
     </div>
 @endsection
 
